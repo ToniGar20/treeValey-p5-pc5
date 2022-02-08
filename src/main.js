effe1b63e4a90dac12af287b8fb7b7f3046305cf
Ignacio <jakrich@cifpfbmoll.eu>
2022-02-08 20:47:20 +0100

    Import Bootstrap

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//Importacion de Bootstrap
+//CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+//JS
+import 'bootstrap'
+
 createApp(App).use(store).use(router).mount('#app')

014527d1363701a02f3dd122c8f975b53d075e86
Toni <garcia.t20@gmail.com>
2022-02-09 20:14:06 +0100

    importing customized css to main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '../css/custom.css'
+
 //Importacion de Bootstrap
 //CSS
 import 'bootstrap/dist/css/bootstrap.min.css'

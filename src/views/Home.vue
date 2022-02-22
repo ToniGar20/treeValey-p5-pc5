120ec81f8c641e84484dfddda303f6baa9c45fe4
Toni <garcia.t20@gmail.com>
2022-02-22 17:08:01 +0100

    carousel work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
 </head>
 <body>
   <Header/>
-<div id="body" class="m-0 m-auto py-5 w-index body-h justify-content-center --bs-body-bg">
+  <div id="body" class="m-0 m-auto py-5 w-index body-h justify-content-center --bs-body-bg">
 		<div class="blog container d-flex flex-lg-row flex-column align-items-md-center overflow-hidden p-1">
 			<div class="w-p">
 				<img src="../assets/house.png" alt="">
@@ -23,24 +23,28 @@
 				</p>
 			</div>
 		</div>
-		<div class="article container d-flex flex-lg-row flex-column-reverse align-items-md-center overflow-hidden pt-5">
-			<div class="float-start w-p text-lg-start text-justify">
-				<h3 class="fs-h3">This is just a place holder</h3>
-				<p>
-					This website template has been designed by <a href="http://www.freewebsitetemplates.com/">Free Website Templates</a> for you, for free.
-				</p>
-				<p>
-					You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to  use this website template without linking back to us.
-				</p>
-				<p>
-					If you're having problems editing this website template, then don't  hesitate to ask for help on the <a href="http://www.freewebsitetemplates.com/forums/">Forums</a>.
-				</p>
-			</div>
-			<div class="w-p">
-				<img src="../assets/nature-place-photo.png" alt="">
-			</div>
-		</div>
-		<div class="news container d-flex flex-lg-row flex-column align-items-md-center overflow-hidden pt-5">
+    <div id="carouselExampleIndicators" class="container carousel slide pt-5 mt-4" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="carrousel d-block mw-100 rounded-4" id="slide1" src="@/assets/deco/deco1.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block mw-100 rounded-4" id="slide2" src="@/assets/deco/deco2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block mw-100 rounded-4" id="slide3" src="@/assets/deco/deco3.jpg" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" v-on:click="prevImg()" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only"></span>
+      </a>
+      <a class="carousel-control-next" role="button" v-on:click="nextImg()" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only"></span>
+      </a>
+    </div>
+		<div class="news container d-flex flex-lg-row flex-column align-items-md-center overflow-hidden pt-5 mt-4">
 			<div class="w-p ms-5" >
 				<img class="ms-5" src="../assets/eco-park.png" alt="">
 			</div>
@@ -56,17 +60,56 @@
 		</div>
 	</div>
 </body>
-  </html>
+</html>
 </template>
 
 <script>
-// @ is an alias to /src
+// @ is an alias to start from /src
 import Header from '@/components/Header.vue'
 
 export default {
   name: 'Home',
+  slideIndex: 0,
   components: {
-    Header
+    Header,
+  },
+  data() {
+    return {
+      slideIndex: 0,
+    }
+  },
+  methods: {
+    prevImg() {
+      if (this.slideIndex !== 0) {
+        let items = document.getElementsByClassName("carousel-item");
+        let currentItem = items[this.slideIndex];
+        currentItem.classList.remove("active");
+        this.slideIndex--;
+
+        let previousItemShown = items[this.slideIndex];
+        previousItemShown.classList.add("active");
+      }
+    },
+    nextImg() {
+      if (this.slideIndex !== 2) {
+        {
+          console.log(this.slideIndex);
+          let items = document.getElementsByClassName("carousel-item");
+          console.log(items);
+          let currentItem = items[this.slideIndex];
+          console.log(currentItem);
+          currentItem.classList.remove("active");
+          this.slideIndex++;
+
+          let nextItemShown = items[this.slideIndex];
+          nextItemShown.classList.add("active");
+        }
+      }
+    },
   }
 }
+
+
+
 </script>
+

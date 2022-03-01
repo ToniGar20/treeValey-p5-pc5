dad05ac32a2c031c261e7b4c1af676f233b7f535
ToniGar20 <tgarcia@habitissimo.com>
2022-03-01 20:09:44 +0100

    imported fontawesome icons

--- src/main.js
+++ src/main.js
@@ -11,4 +11,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 //JS
 import 'bootstrap'
 
+//Fontawesome icons import
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+
 createApp(App).use(store).use(router).mount('#app')

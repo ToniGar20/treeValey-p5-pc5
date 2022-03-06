e62289c88e0931626ff770de71c1e764933e1271
Ignacio <jakrich@cifpfbmoll.eu>
2022-03-06 19:26:46 +0100

    prueba img silla

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ export default createStore({
     productos: [
       {
         id:1,
-        imagen:'/img/Silla-e.72a7988f.webp',
+        imagen:'/img/Silla-e.webp',
         nombre: 'Silla',
         descripcion: 'Silla de madera para exteriores, comoda y resistente al sol.',
         price: 100,
